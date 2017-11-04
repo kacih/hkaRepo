0f38cba155f03dc8ca6d88939214485c4b39e3f2
habib <khabib_dz@yahoo.fr>
2017-11-04 11:51:44 +0100

    test U OK, calcul de différence entre deux dates

--- firstVC/src/firstVC/Developer.java
+++ firstVC/src/firstVC/Developer.java
@@ -1,7 +1,13 @@
 package firstVC;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Developer {
 
@@ -28,10 +34,25 @@ public class Developer {
 
 	}
 	
-	private long calculateDifferenceBetweenTwoDates(Date dateFrom, Date dateFin)
+	private long calculateDifferenceBetweenTwoDates(Date dateFrom, Date datTo)
 	{
 		
-		return 0;
+			Calendar calFrom = Calendar.getInstance();
+			calFrom.setTime(dateFrom);
+			Calendar calTo = Calendar.getInstance();
+			calTo.setTime(datTo);
+			return calTo.compareTo(calFrom);		
+	}
+	
+	@Test
+	public void testDifferenceEntreDeuxDate() throws ParseException
+	{
+		SimpleDateFormat spdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date datTo = spdf.parse("31/08/1978");
+		Date dateFrom = spdf.parse("11/06/1976");
+		
+		Assert.assertFalse("résultat de calcul "+ calculateDifferenceBetweenTwoDates(dateFrom, datTo),
+				calculateDifferenceBetweenTwoDates(dateFrom, datTo) > 0);
 	}
 	
 	
